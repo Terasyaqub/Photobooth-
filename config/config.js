c12e9114d7761df72498a692c7ae1edc5a4e4059
Terasyaqub <terasyaqub.banjarmasin19@gmail.com>
2026-09-11 07:21:48 +0800

    Add configuration for Supabase and storage bucket

---
+++ config/config.js
@@ -0,0 +1,6 @@
+window.TERASYAQUB_CONFIG = {
+    SUPABASE_URL: "https://wiyeeablhbxzhxgrijbt.supabase.co",
+    SUPABASE_ANON_KEY: "sb_publishable_pYKvK6_rn8Jrej9RtOasAw_0Yu9Y1ef",
+
+    STORAGE_BUCKET: "photobooth"
+};
