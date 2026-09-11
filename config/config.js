02a9c85c4bec120957d87d4a67cd4f7b7d8f87d3
Terasyaqub <terasyaqub.banjarmasin19@gmail.com>
2026-09-11 10:58:11 +0800

    Update Supabase configuration

--- config/config.js
+++ config/config.js
@@ -1,6 +1,15 @@
 window.TERASYAQUB_CONFIG = {
-    SUPABASE_URL: "https://wiyeeablhbxzhxgrijbt.supabase.co",
-    SUPABASE_ANON_KEY: "sb_publishable_pYKvK6_rn8Jrej9RtOasAw_0Yu9Y1ef",
+  SUPABASE_URL: "https://wiyeeablhbxzhxg.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_pYKvK6_rn8Jrej9RtOasAw_0Yu9Y1ef",
 
-    STORAGE_BUCKET: "photobooth"
+  STORAGE_BUCKET: "photobooth",
+
+  PRINT_MODE: "browser",
+
+  PRINT_BRIDGE_URL: "",
+
+  PRINTER_NAME: "EPSON L3251 Series",
+
+  APP_NAME: "TERASYAQUB PHOTOBOOTH",
+  BRAND: "PHOTOBOOTH TERASYAQUB"
 };
